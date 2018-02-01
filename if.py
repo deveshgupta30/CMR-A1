1221e88fa3982fcdeadf8d82fd01b8693a9a07a9
Devesh Gupta <10222924+deveshgupta30@users.noreply.github.com>
2018-01-31 22:16:05 -0800

    Add files via upload

---
+++ if.py
@@ -0,0 +1,31 @@
+#if
+z=1
+if z==1:
+    print("True")
+
+
+#if ladder
+a=int(input("Enter a value:"))
+if a>0:
+    print("Positive")
+if a<0:
+    print("Negative")
+
+
+#if-else
+b=int(input("Enter another value:"))
+if (b==0):
+    print("Even")
+else:
+    print("Odd")
+
+#if-elif-else
+q=1
+if q==1:
+    print("q is intact")
+elif q>=1:
+    print("q is intact")
+elif q<=1:
+    print("q is intact")
+else:
+    print("q is in danger")
---
+++ input.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number:")) #for integer explicit type conv.
+b=int(input("Enter another number:"))
+c=a+b
+d=1
+e=2
+e*=c
+d+=c
+print(c)
+print(d)
+print(e)
+f=input("One More...") 
+g=input("Last time:")
+h=int(f)+int(g)
+print(h)
+# normal a+b can be used for string concat
+print('p')
